314607454724abbf40be487d6b02b2bfd8394a42
Amin_shdl <135274717+iaamin@users.noreply.github.com>
2023-07-01 18:13:57 +0330

    app.js
    app.js added!

---
+++ app.js
@@ -0,0 +1,39 @@
+import { Component, component } from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+//imrc: import React, { Component } from 'react';
+// cc: class App extends Component {
+//     state = {  } 
+//     render() { 
+//         return ();
+//     }
+// }
+// sfc:  const App = () => {
+//     return (  );
+//  }
+  
+//  export dsfc: efault ;
+
+
+
+class App extends Component {
+     render() { 
+     return (
+         <>
+           <h1>Hello My friend</h1><br />
+           <h2>this is react my friend</h2>
+
+         </>
+     )   
+     }
+ }
+
+export default App;
+
+// function App() {
+//     return (
+//         <>
+//        <h1>Hello My friend</h1><br />
+//        <h2>this is react my friend</h2>
+//         </>
+//     )
+// }
